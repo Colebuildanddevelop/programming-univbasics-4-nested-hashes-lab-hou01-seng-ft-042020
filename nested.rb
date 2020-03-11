08bfa5e55fea0b1adfd8467b5973399057f003b5
Cole Davis <colebuildanddevelop@gmail.com>
2020-03-11 21:21:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,6 +132,15 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-  puts programmer_hash[:dennis_ritchie][:languages] << ("Assembly")
+  programmer_hash[:dennis_ritchie][:languages] << ("Assembly")
+
+  puts programmer_hash[:dennis_ritchie][:languages]
+  
 end
-a
+
+
+
+
+
+
+
